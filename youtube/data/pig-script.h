7a2ced68fbc2e5667875c2ae3f8e793399bfc351
ahmadsabir23 <38085412+ahmadsabir23@users.noreply.github.com>
2018-04-09 20:32:45 +0100

    Create pig-script.h

---
+++ youtube/data/pig-script.h
@@ -0,0 +1,22 @@
+-- Registring piggybank
+REGISTER /home/vagrant/assignment1/piggybank-0.12.0.jar
+
+-- Loading five diffrent CSV files into PIG
+CAdata = LOAD '/home/vagrant/youtube/CAvideos.csv' USING org.apache.pig.piggybank.storage.CSVExcelStorage(',', 'NO_MULTILINE', 'UNIX', 'SKIP_INPUT_HEADER');
+
+DEdata = LOAD '/home/vagrant/youtube/DEvideos.csv' USING org.apache.pig.piggybank.storage.CSVExcelStorage(',', 'NO_MULTILINE', 'UNIX', 'SKIP_INPUT_HEADER');
+
+FRdata = LOAD '/home/vagrant/youtube/FRvideos.csv' USING org.apache.pig.piggybank.storage.CSVExcelStorage(',', 'NO_MULTILINE', 'UNIX', 'SKIP_INPUT_HEADER');
+
+GBdata = LOAD '/home/vagrant/youtube/GBvideos.csv' USING org.apache.pig.piggybank.storage.CSVExcelStorage(',', 'NO_MULTILINE', 'UNIX', 'SKIP_INPUT_HEADER');
+
+USdata = LOAD '/home/vagrant/youtube/USvideos.csv' USING org.apache.pig.piggybank.storage.CSVExcelStorage(',', 'NO_MULTILINE', 'UNIX', 'SKIP_INPUT_HEADER');
+
+-- Combining all CSV files
+U = UNION CAdata, DEdata, FRdata, GBdata, USdata;
+
+-- Selecting limited number of columns 
+limitedData = FOREACH U GENERATE $0 .. $3, $5 .. $10, $14;
+
+-- storing data
+STORE limitedData into '/home/vagrant/youtube/data';
